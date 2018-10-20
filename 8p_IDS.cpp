4b8f0b049055a8e6675dc99dd3431c2f3266ec42
handery <43126074+handery@users.noreply.github.com>
2018-10-20 11:36:31 +0800

    Update 8p_IDS.cpp

--- 8p_IDS.cpp
+++ 8p_IDS.cpp
@@ -1,24 +1,23 @@
-#include "pch.h"
 #include<iostream>
-#include<algorithm>
 #include<map>
 #include<string>
 #include<stack>
 #include<fstream>
+#include<time.h>
 using namespace std;
 
-const string dest = "123456780";
-map<string, bool> nmap;
+const string dest = "123456780";			//目标状态
+map<string, bool> nmap;						//剔除节点，以防重复搜索
 
 const int gmove[9][4] = {
 	{-1,3,-1,1	},{-1,4,0,2},{-1,5,1,-1},
 	{0,6,-1,4	},{1,7,3,5}	,{2,8,4,-1},
 	{3,-1,-1,7	},{4,-1,6,8},{5,-1,7,-1}
-};
+};											//节点上下移动
 
 int getPos(string num) {
 	return num.find('0');
-}
+}											//找0的位置
 
 string swap(string num, int a, int b) {
 	int pos = num.find('0');
@@ -26,7 +25,7 @@ string swap(string num, int a, int b) {
 	num[a] = num[b];
 	num[b] = c;
 	return num;
-}
+}											//交换位置、移动
 
 bool has_solution(string num) {
 	int que = stoi(num);
@@ -46,7 +45,7 @@ bool has_solution(string num) {
 		}
 	}
 	return sum % 2 == 0;
-}
+}											//看这个输入是否有解
 
 int dfs(string init, int step, int max_depth, stack<string>& path) {
 	if (init == dest)
@@ -61,7 +60,8 @@ int dfs(string init, int step, int max_depth, stack<string>& path) {
 				continue;
 			}
 			nmap[next] = 1;
-			path.push(next+" to position "+to_string(pos));
+			string movement[4] = { "up  ","down","left ","right" };
+			path.push(next +"   "+movement[i] +" to position " + to_string(pos));
 			int ans = dfs(next, step + 1, max_depth, path);
 			if (ans == 1)
 				return 1;
@@ -70,25 +70,24 @@ int dfs(string init, int step, int max_depth, stack<string>& path) {
 		}
 	}
 	return -1;
-}
+}											//搜索
 
 void getp(stack<string> path) {
-	//cout << path.top() << endl;
 	stack<string> t;
 	while (!path.empty()) {
 		t.push(path.top());
 		path.pop();
 	}
-	ofstream outfile("out.txt");
+	ofstream outfile("out_IDS.txt");
 	while (!t.empty()) {
 		outfile << t.top() << endl;
 		t.pop();
 	}
-}
+}											//输出路径
 
 void IDS(string initial) {
 	if (!has_solution(initial)) {
-		cout << "No solution."<<endl;
+		cout << "No solution." << endl;
 		return;
 	}
 	int max_step = 1000;
@@ -110,8 +109,19 @@ void IDS(string initial) {
 }
 
 int main() {
-	string first = "562870134";
-	IDS(first);
-
+	string first = "503624871";
+	clock_t startTime, endTime;
+	int loop_time = 5;
+	double total_time = 0;
+	while (loop_time--) {
+		startTime = clock();
+		IDS(first);
+		endTime = clock();
+		total_time += (double)(endTime - startTime) / CLOCKS_PER_SEC;
+	}
+	ofstream outfile("out_IDS.txt",ios::app);
+	outfile << "Average_time: " << total_time / 5 << "s" << endl;
 
 }
+
+//{562870134,876543210,345201876,852014637,503624871}
